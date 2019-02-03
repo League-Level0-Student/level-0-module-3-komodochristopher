457c1b0006a01e2b20ab93c7680de7ca38f00b98
komodochristopher <league@WTS13.attlocal.net>
2019-02-02 17:26:32 -0800

    are you happy

--- src/elseif/areyouhappy.java
+++ src/elseif/areyouhappy.java
@@ -2,18 +2,35 @@ package elseif;
 
 import javax.swing.JOptionPane;
 
+
+
+
+
 public class areyouhappy {
 	public static void main(String[] args) {
-		int x = JOptionPane.showConfirmDialog(null, "are you happy?");
-		if (x == 1) {
-			x=JOptionPane.showConfirmDialog(null, "do you want to be happy?");
-			if (x == 0) {
-				JOptionPane.showMessageDialog(null, "change something");
-				
-			 if (x==0) {
-					JOptionPane.showMessageDialog(null, "keep doing whatever you're doing");
-				}
-			}
+
+		String happy = JOptionPane.showInputDialog(null, "are you happy?");
+
+		
+		
+		if (happy.equals("no")) {
+			JOptionPane.showInputDialog(null, "do you want to be happy?");
 		}
+		
+		else if (happy.equals("yes")) {
+			JOptionPane.showMessageDialog(null, "change something");
+		}
+
+		else if (happy.equals("no")) {
+			JOptionPane.showMessageDialog(null, "keep doing whatever you are doing ");
+		}
+		
+	
+	
+	
+	
+	
+	
+	
 	}
 }
