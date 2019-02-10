5383008e3d2f7c99b6a4d1a3e4741c5ed7c7a0dc
Dong Zou <jiwenzou@gmail.com>
2019-02-09 17:21:44 -0800

    are you happy

--- src/elseif/areyouhappy.java
+++ src/elseif/areyouhappy.java
@@ -2,35 +2,29 @@ package elseif;
 
 import javax.swing.JOptionPane;
 
-
-
-
-
 public class areyouhappy {
+
 	public static void main(String[] args) {
 
 		String happy = JOptionPane.showInputDialog(null, "are you happy?");
 
-		
-		
-		if (happy.equals("no")) {
-			JOptionPane.showInputDialog(null, "do you want to be happy?");
-		}
-		
-		else if (happy.equals("yes")) {
-			JOptionPane.showMessageDialog(null, "change something");
+		if (happy.equals("yes")) {
+			JOptionPane.showMessageDialog(null, "keep doing whatever your doing.");
 		}
 
-		else if (happy.equals("no")) {
-			JOptionPane.showMessageDialog(null, "keep doing whatever you are doing ");
+		if (happy.equals("no")) {
+			happy = JOptionPane.showInputDialog(null, "do you want to be happy?");
+
+			if (happy.equals("yes")) {
+				JOptionPane.showMessageDialog(null, "change something");
+			} else {
+
+				// if (happy.equals("yes")) {
+				JOptionPane.showMessageDialog(null, "keep doing whatever your doing.");
+				// }
+
+			}
+
 		}
-		
-	
-	
-	
-	
-	
-	
-	
 	}
 }
---
+++ src/horoscope.java
@@ -0,0 +1,9 @@
+
+public class horoscope {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
