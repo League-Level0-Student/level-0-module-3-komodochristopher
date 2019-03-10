338d69fb09af016615b5adfe4dd597d474398317
komodochristopher <league@WTS20.attlocal.net>
2019-03-09 17:24:01 -0800

    shiny objects years alive

---
+++ src/extra/YearsAlive.java
@@ -0,0 +1,12 @@
+package extra;
+
+public class YearsAlive {
+public static void main(String[] args) {
+	for(int i=2006; i<2020; i++) {
+	System.out.println(i);
+	
+
+}
+
+}
+}
--- src/loops/ShinyObjects.java
+++ src/loops/ShinyObjects.java
@@ -8,17 +8,25 @@ import java.applet.AudioClip;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JApplet;
-
+import javax.swing.JOptionPane;
 
 public class ShinyObjects {
 	public static void main(String[] args) {
 		// 2. Ask the user how many shiny objects they want
+		String answer = JOptionPane.showInputDialog(null, "how many shiny objects do you want?");
+		int x = Integer.parseInt(answer);
 
-		// 3. Play the sound that many times
-
+			// 3. Play the sound that many times
+			for (int i = 0; i < x; i++) {
+				playMisterZee();
+			
+			
+	
+			}
+		}
 		// 1. Call the method below
 
-	}
+
 
 	public static void playMisterZee() {
 		try {
@@ -29,6 +37,5 @@ public class ShinyObjects {
 			ex.printStackTrace();
 		}
 	}
-
 
 }
